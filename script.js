e4c30262e3f5ffd764ad6f8c1115ce04213e5a90
dmitry.darevsky <darevsky.dmitry@gmail.com>
2020-10-23 15:20:13 +0300

    work with CSS

--- script.js
+++ script.js
@@ -2,18 +2,34 @@ var vue = new Vue ({
   el: '#app',
   data: {
     count: 0,
+
     url: '',
-    cleanUrl: ''
+    cleanUrl: '',
+    
+    title: 'Style',
+    sizeToggle: false,
+    isRounded: false,
+    disabled: false,
+    fontColor: 'grey',
+    backgroundColor: "light-blue"
   },
   methods: {
     countUp: function () {
-      this.count += 1
+      this.count += 1;
     },
     countDown: function () {
-      this.count -= 1
+      this.count -= 1;
     },
     cleanerUrl: function () {
-      this.cleanUrl = this.url.replace(/^https?:\/\//, '').replace(/\/$/, '')
+      this.cleanUrl = this.url.replace(/^https?:\/\//, '').replace(/\/$/, '');
     }
-  } 
-})
+  },
+  computed: {
+    styles: function () {
+      return {
+        color: this.fontColor,
+        background: this.backgroundColor
+      };
+    }
+  }
+}); 
